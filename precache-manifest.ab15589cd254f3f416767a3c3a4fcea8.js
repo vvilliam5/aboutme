3d98fb05e59d548d7fdac4ff66779f9a8c8e5d17
williamswill <willoymaniak@gmail.com>
2019-12-10 16:54:59 +0100

    Updates

---
+++ precache-manifest.ab15589cd254f3f416767a3c3a4fcea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a8e98dbaed82a767b8f74555711010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e594ec75d997c0d51e6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a0b836e090910163b943",
+    "url": "/static/js/2.0ec32d5c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0ec32d5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e594ec75d997c0d51e6",
+    "url": "/static/js/main.956dedcf.chunk.js"
+  },
+  {
+    "revision": "3adf6062801dd9924057",
+    "url": "/static/js/runtime-main.e9aa8685.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
